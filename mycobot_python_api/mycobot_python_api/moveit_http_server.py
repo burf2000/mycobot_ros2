2a26ecb14f91f1b7f9e04c0fc35eb28b1c28f842
Simon Burfield <sb@compsoft.com>
2025-07-14 11:11:38 +0100

    Fixed Api

--- mycobot_python_api/mycobot_python_api/moveit_http_server.py
+++ mycobot_python_api/mycobot_python_api/moveit_http_server.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 import rclpy
+
 from flask import Flask, request, jsonify
 from geometry_msgs.msg import PoseStamped
 
 # moveit python library
-from moveit.core.robot_state import RobotState
 from moveit.planning import (
     MoveItPy,
-    MultiPipelinePlanRequestParameters,
 )
 
 app = Flask(__name__)
 
 moveit: MoveItPy = None
 planning_component = None
+
 
 @app.route("/move_to_xyz", methods=["POST"])
 def move_to_xyz():
@@ -22,36 +22,22 @@ def move_to_xyz():
     if None in (x, y, z):
         return jsonify({"error": "Missing x/y/z"}), 400
 
-    goal = PoseStamped()
-    goal.header.frame_id = "base_link"
-    goal.pose.position.x = x
-    goal.pose.position.y = y
-    goal.pose.position.z = z
-    goal.pose.orientation.w = 1.0
+    pose_goal = PoseStamped()
+    pose_goal.header.frame_id = "base_link"
+    pose_goal.pose.orientation.w = 1.0
+    pose_goal.pose.position.x = x
+    pose_goal.pose.position.y = y
+    pose_goal.pose.position.z = z
+    planning_component.set_goal_state(pose_stamped_msg=pose_goal, pose_link="gripper_base")
 
-    planning_component.set_goal_state(pose_stamped_msg=goal,
-                                      pose_link="gripper_base")
+    plan_result = planning_component.plan()
 
-    # params = PlanRequestParameters(moveit, "ompl")  # MoveItPy instance + pipeline
-    # params.planning_time = 5.0                      # 5 s planning timeout
+    robot_trajectory = plan_result.trajectory
 
-    # initialise multi-pipeline plan request parameters
-    multi_plan_parameters = MultiPipelinePlanRequestParameters(
-            moveit, ["ompl_rrtc", "pilz_lin", "chomp", "ompl_rrt_star"]
-    )
-
-    plan_result = planning_component.plan(
-        multi_plan_parameters=multi_plan_parameters
-    )
-
-    # 🚨  POSitional call – no keyword
-    #plan_result = planning_component.plan()
-
-    if plan_result and plan_result.trajectory:
-        planning_component.execute()
+    if plan_result and robot_trajectory:
+        moveit.execute(robot_trajectory, controllers=[])
         return jsonify({"status": "success"}), 200
     return jsonify({"status": "planning_failed"}), 500
-
 
 def main():
     global moveit, planning_component
