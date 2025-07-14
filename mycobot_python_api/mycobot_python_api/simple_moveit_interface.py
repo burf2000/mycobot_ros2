de3766c20e8a85699723df6e166019d97d6f0a17
Simon Burfield <sb@compsoft.com>
2025-07-14 10:44:31 +0100

    Something works

--- mycobot_python_api/mycobot_python_api/simple_moveit_interface.py
+++ mycobot_python_api/mycobot_python_api/simple_moveit_interface.py
@@ -37,6 +37,7 @@ def plan_and_execute(
             single_plan_parameters=single_plan_parameters
         )
     else:
+        logger.info("**** Using just plan")
         plan_result = planning_component.plan()
 
     # execute the plan
@@ -68,22 +69,24 @@ def main():
     # Plan 2 - set goal state with RobotState object
     ###########################################################################
 
-    # instantiate a RobotState instance using the current robot model
-    robot_model = panda.get_robot_model()
-    robot_state = RobotState(robot_model)
-
-    # randomize the robot state
-    robot_state.set_to_random_positions()
-
     # set plan start state to current state
     panda_arm.set_start_state_to_current_state()
 
-    # set goal state to the initialized robot state
-    logger.info("Set goal state to the initialized robot state")
-    panda_arm.set_goal_state(robot_state=robot_state)
+    # set pose goal with PoseStamped message
+    from geometry_msgs.msg import PoseStamped
+
+    pose_goal = PoseStamped()
+    pose_goal.header.frame_id = "base_link"
+    pose_goal.pose.orientation.w = 1.0
+    pose_goal.pose.position.x = -0.2
+    pose_goal.pose.position.y = 0.001
+    pose_goal.pose.position.z = 0.009
+    panda_arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="gripper_base")
 
     # plan to goal
     plan_and_execute(panda, panda_arm, logger, sleep_time=3.0)
+
+
 
     ###########################################################################
     # Plan 3 - set goal state with PoseStamped message
@@ -96,16 +99,15 @@ def main():
     from geometry_msgs.msg import PoseStamped
 
     pose_goal = PoseStamped()
-    pose_goal.header.frame_id = "link1_to_link2"
+    pose_goal.header.frame_id = "base_link"
     pose_goal.pose.orientation.w = 1.0
-    pose_goal.pose.position.x = 0.02
-    pose_goal.pose.position.y = -0.2
-    pose_goal.pose.position.z = 0.2
+    pose_goal.pose.position.x = 0.2
+    pose_goal.pose.position.y = 0.002
+    pose_goal.pose.position.z = 0.002
     panda_arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="gripper_base")
 
     # plan to goal
     plan_and_execute(panda, panda_arm, logger, sleep_time=3.0)
-
 
 
 if __name__ == "__main__":
