393713934b6a22d87a4f0cdcc9584904725ed8e2
Simon Burfield <sb@compsoft.com>
2025-07-14 14:42:49 +0100

    Working gripper now

--- mycobot_python_api/mycobot_python_api/moveit_http_server.py
+++ mycobot_python_api/mycobot_python_api/moveit_http_server.py
@@ -3,7 +3,10 @@ import rclpy
 
 from flask import Flask, request, jsonify
 from geometry_msgs.msg import PoseStamped
-
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from moveit.core.robot_state import RobotState
+from moveit.core.kinematic_constraints import construct_joint_constraint
 # moveit python library
 from moveit.planning import (
     MoveItPy,
@@ -13,6 +16,34 @@ app = Flask(__name__)
 
 moveit: MoveItPy = None
 planning_component = None
+
+
+
+
+@app.route("/gripper/<action>", methods=["POST"])
+def gripper(action):
+    if action not in ("open", "close"):
+        return jsonify(error="Invalid action"), 400
+
+
+
+    # # Create RobotState and set gripper joint(s) value
+    robot_model = moveit.get_robot_model()
+    st = RobotState(robot_model)
+
+    if action == "open":
+        planning_component_gripper.set_goal_state(configuration_name="open")
+    else:
+        planning_component_gripper.set_goal_state(configuration_name="closed")
+
+
+
+    plan_result = planning_component_gripper.plan()
+    if plan_result and plan_result.trajectory:
+        moveit.execute(plan_result.trajectory, controllers=[])
+        return jsonify(status="success"), 200
+
+    return jsonify(status="planning_failed"), 500
 
 
 @app.route("/move_to_xyz", methods=["POST"])
@@ -40,10 +71,11 @@ def move_to_xyz():
     return jsonify({"status": "planning_failed"}), 500
 
 def main():
-    global moveit, planning_component
+    global moveit, planning_component, planning_component_gripper
     rclpy.init()
     moveit = MoveItPy(node_name="moveit_http_server")   # loads MoveIt params
     planning_component = moveit.get_planning_component("arm")
+    planning_component_gripper = moveit.get_planning_component("gripper")
     app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
